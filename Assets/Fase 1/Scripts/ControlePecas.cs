fb0237932b1d723423df71587d016429eeb18d43
Will00000000 <rm94807@estudante.fieb.edu.br>
2025-05-21 10:00:18 -0300

    V45

---
+++ Assets/Fase 1/Scripts/ControlePecas.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlePecas : MonoBehaviour
+{
+    public RectTransform sombra;
+    RectTransform peca;
+    
+    void Awake ()
+    {
+        peca = GetComponent <RectTransform> ();
+        peca.sizeDelta = sombra.sizeDelta;
+    }
+}
--- Assets/Fase 1/Scripts/Drop.cs
+++ Assets/Fase 1/Scripts/Drop.cs
@@ -5,11 +5,21 @@ using UnityEngine.EventSystems;
 
 public class Drop : MonoBehaviour, IDropHandler
 {
+    //string nomeTag;
+    
     public void OnDrop (PointerEventData eventData)
     {
         if (eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent <RectTransform> ().anchoredPosition = GetComponent <RectTransform> ().anchoredPosition;
+            if (eventData.pointerDrag.gameObject.tag.Equals(gameObject.tag))
+            {
+                eventData.pointerDrag.GetComponent <RectTransform> ().anchoredPosition = GetComponent <RectTransform> ().anchoredPosition;
+            }
         }
     }
+
+    /*void Start ()
+    {
+        nomeTag = gameObject.tag;
+    }*/
 }
